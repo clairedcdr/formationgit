fe6845a9b8c8a990ea8546cfac1d64f26a13ce38
Du Campe De Rosamel Claire <P5BOPG@ad.insee.intra>
2022-10-04 10:50:05 +0200

    changement fonction

--- scripts/script3.R
+++ scripts/script3.R
@@ -1,7 +1,13 @@
 a <- function(x)
-  {1 + ma_function_mieux(x)$maximum
+  {1 + ma_function_stats(x)$maximum
 }
-a(c(1,2,3,4,5,6,7,8,9,11,12,13,14))
+a_result = a(c(1,2,3,4,5,6,7,8,9,11,12,13,14))
 
-b <- function(x){a(x)*ma_function_mieux(x)$mediane}
-b(c(1,2,3,4,5,6,7,8,9,11,12,13,14))
+b <- function(x){a(x)*ma_function_stats(x)$mediane}
+b_result = b(c(1,2,3,4,5,6,7,8,9,11,12,13,14))
+
+c=a_result +b_result
+c
+
+class(a_result)
+class(b_result)
